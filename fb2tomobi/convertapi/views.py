df9d914cd836717dc8797ccb7091dbb8f5e85645
Gatis Rīders <gatis.riders@elkogroup.com>
2020-10-05 21:11:23 +0300

    remove commented out code

--- fb2tomobi/convertapi/views.py
+++ fb2tomobi/convertapi/views.py
@@ -12,20 +12,3 @@ class BookViewSet(viewsets.ModelViewSet):
     parser_classes = [JSONParser]
     def post(self, request, format=None):
         return response.Response({'received data': request.data})
-    # @action(
-    #     detail=True,
-    #     methods=['PUT'],
-    #     serializer_class=BookFileSerializer,
-    #     parser_classes=[parsers.MultiPartParser],
-    # )
-    # def book(self, request, pk):
-    #     obj = self.get_object()
-    #     serializer = self.serializer_class(obj, data=request.data,
-    #                                        partial=True)
-    #     if serializer.is_valid():
-    #         serializer.save()
-    #         return response.Response(serializer.data)
-    #     return response.Response(serializer.errors,
-    #                              status.HTTP_400_BAD_REQUEST)
-    # queryset = Book.objects.all().order_by('name')
-    # serializer_class = BookSerializer
