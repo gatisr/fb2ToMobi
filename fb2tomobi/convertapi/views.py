7e6856053b84feafb80998f346658d3b7785b85a
Gatis Rīders <gatis.riders@elkogroup.com>
2020-10-10 14:01:57 +0300

    FPP2: removed code leftovers

--- fb2tomobi/convertapi/views.py
+++ fb2tomobi/convertapi/views.py
@@ -9,13 +9,3 @@ class BookViewSet(viewsets.ModelViewSet):
     serializer_class = BookSerializer
     queryset = Book.objects.all().order_by('name')
     parser_classes = [MultiPartParser]
-
-    def post(self, request):
-        file = request.data.get('book')
-        if file and not file.name.endswith('.fb2'):
-            return Response({'errors': "file type is not supported"}, status=400)
-        file_serializer = BookSerializer(data=request.data)
-        if file_serializer.is_valid():
-            file_serializer.save()
-            return Response(file_serializer.data, status=status.HTTP_201_CREATED)
-        return Response(file_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
